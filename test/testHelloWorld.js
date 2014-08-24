7d8fdac443f09e4a0fc33e9df0810727fea53383
Thomas Junghans <thomas.junghans@gmail.com>
2014-08-24 21:39:59 +0200

    test: update tests

--- test/testHelloWorld.js
+++ test/testHelloWorld.js
@@ -1,12 +1,44 @@
 'use strict';
 
-var webdriver = require('selenium-webdriver');
+//https://github.com/vvo/selenium-standalone
 
-var driver = new webdriver.Builder().
-    withCapabilities(webdriver.Capabilities.chrome()).
-    build();
-
-driver.get('http://www.commercia-sh.ch');
+var webdriverjs = require('webdriverjs');
+var assert = require('assert');
 
 
-driver.quit();
+// Docu: https://code.google.com/p/selenium/source/browse/javascript/webdriver/webdriver.js
+
+describe('HSR Startpage', function () {
+	this.timeout(99999999);
+	var client = {};
+
+	before(function (done) {
+		client = webdriverjs.remote({desiredCapabilities: { browserName: 'phantomjs' }} );
+            client.init(done);
+	});
+
+	it('has title', function (done) {
+	
+		client
+			.url('http://www.hsr.ch/')
+			.getTitle(function(err, title) {
+                 		assert(err === null);
+                 		assert(title === 'HSR Hochschule für Technik Rapperswil: Home');
+             	})
+			.call(done);
+	});
+
+	it('has a logo', function (done) {
+	
+		client
+			.url('http://www.hsr.ch/')
+			.getTagName ('#logo', function(err, tagName)  {
+				console.log(tagName, err);
+			});
+			.call(done);
+	});
+
+	after(function(done) {
+        	client.end(done);
+    	});
+});
