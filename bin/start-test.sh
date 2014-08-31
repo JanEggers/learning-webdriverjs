b32907b7e33eeb10d450478f2f90798b004394ca
Thomas Junghans <thomas.junghans@gmail.com>
2014-08-31 19:11:01 +0200

    chore: add stylus and update build scripts

--- bin/start-test.sh
+++ bin/start-test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+
 (rm ./selenium.log*)
 (start-selenium -log ./selenium.log & npm start & npm run watch-test) 
---
+++ bin/watch-css.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+(stylus --watch component/css/src/main.styl --out component/css/static --compress)
